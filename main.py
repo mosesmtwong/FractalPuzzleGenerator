1d50ff70e08f29f55105ca7cd0d07f5827178ec5
rem-lover <moseswong1010@gmail.com>
2024-05-11 19:27:46 +0800

    can gen hexagon

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class node:
 draw = ImageDraw.Draw(im)
 
 parity = 0
-st_x, st_y = 0, 0
+st_x, st_y = 0, 1
 for y in range(st_y, HEIGHT, ROOT):
     parity += 1
     if parity % 2 == 1:
@@ -34,8 +34,13 @@ for y in range(st_y, HEIGHT, ROOT):
     else:
         st_x -= UNIT // 2
     for x in range(st_x, SIDE * UNIT * 2, UNIT):
-        if y <= INV_A * x + D * ROOTTHREETWO:
-            im.putpixel((x, y), (0, 0, 0))
+        if (
+            y <= INV_A * x + D * ROOTTHREETWO - 1
+            and y >= -INV_A * x + D * ROOTTHREETWO + 1
+            and y <= -INV_A * (x - 2 * D) + D * ROOTTHREETWO - 1
+            and y >= INV_A * (x - 2 * D) + D * ROOTTHREETWO + 1
+        ):
+            draw.ellipse(((x - 4, y - 4), (x + 4, y + 4)), (0, 0, 0))
 
 
 im.save("puzzle.png")
