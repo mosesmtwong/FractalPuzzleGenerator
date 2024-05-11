60561464765c0537d8d59a82de4973ef3cd01421
rem-lover <moseswong1010@gmail.com>
2024-05-11 19:27:37 +0800

    can gen hexagon

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 from PIL import Image, ImageDraw
-import math
+from math import *
 
 SIDE = 15
-UNIT = 10
-ROOT = int(math.sqrt(3) * UNIT // 2)
-print(ROOT)
+UNIT = 100
+ROOT = round(sqrt(3) * UNIT // 2)
+HEIGHT = round(UNIT * SIDE * sqrt(3))
+INV_A = 1 / tan(pi / 6)
+ROOTTHREETWO = sqrt(3) / 2
+D = UNIT * SIDE
 
-im = Image.new("RGB", (200, 200), (255, 255, 255))
+im = Image.new("RGB", (SIDE * UNIT * 2, HEIGHT), (255, 255, 255))
 
 
 class node:
-    def __init__(self, x, y, left, right, upleft, upright, downleft, downright):
+    def __init__(self, x, y):
         self.coords = (x, y)
-        self.left = left
-        self.right = right
-        self.upleft = upleft
-        self.upright = upright
-        self.downleft = downleft
-        self.downright = downright
+        self.left = 1
+        self.right = 1
+        self.upleft = 1
+        self.upright = 1
+        self.downleft = 1
+        self.downright = 1
 
-
-pointA = node(100, 100, 0, 0, 0, 0, 0, 0)
 
 draw = ImageDraw.Draw(im)
 
 parity = 0
-st_x, st_y = 10, 10
-for y in range(st_y, SIDE * ROOT, ROOT):
+st_x, st_y = 0, 0
+for y in range(st_y, HEIGHT, ROOT):
     parity += 1
     if parity % 2 == 1:
         st_x += UNIT // 2
     else:
         st_x -= UNIT // 2
-    for x in range(st_x, SIDE * UNIT, UNIT):
-        im.putpixel((x, y), (0, 0, 0))
+    for x in range(st_x, SIDE * UNIT * 2, UNIT):
+        if y <= INV_A * x + D * ROOTTHREETWO:
+            im.putpixel((x, y), (0, 0, 0))
 
 
 im.save("puzzle.png")
